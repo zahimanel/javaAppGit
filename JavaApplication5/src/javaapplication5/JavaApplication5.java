4e68aa2c0a9d164d123b016071704d1b3ddb6117
zahimanel <zahimanel@yahoo.fr>
2014-02-18 10:40:39 +0100

    1er commit

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication5 {
+    private int id;
+    private String attribut1;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
